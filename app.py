f20d77299cc558662a90ae6541f6c014b5516182
Al-Fayez-med <arch.fa.hm@gmail.com>
2026-04-12 18:54:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -10,23 +10,25 @@ st.markdown("""
     background: radial-gradient(circle at center, #3b82f6 0%, #1e3a8a 70%);
 }
 
-/* زر الإيموجي */
+/* زر مربع */
 .stButton > button {
-    width: 100%;
+    width: 90px;
     height: 90px;
-    font-size: 40px; /* 👈 كبرنا الأيقونة */
+    font-size: 40px;
     border-radius: 20px;
     border: 2px solid rgba(255,255,255,0.5);
     background: rgba(255,255,255,0.05);
+    display: block;
+    margin: auto;
 }
 
 /* النص */
 .label {
     text-align: center;
     color: white;
-    margin-top: -5px;
-    margin-bottom: 10px;
     font-size: 14px;
+    margin-top: 5px;
+    margin-bottom: 20px;
 }
 
 </style>
@@ -43,20 +45,29 @@ if st.session_state.page == "home":
 
     with center:
 
-        if st.button("📦"):
-            st.session_state.page = "products"
-            st.rerun()
-        st.markdown('<div class="label">الأصناف</div>', unsafe_allow_html=True)
+        # ===== أيقونة 1 =====
+        c1, c2, c3 = st.columns(3)
+        with c2:
+            if st.button("📦"):
+                st.session_state.page = "products"
+                st.rerun()
+            st.markdown('<div class="label">الأصناف</div>', unsafe_allow_html=True)
 
-        if st.button("🗂️"):
-            st.session_state.page = "categories"
-            st.rerun()
-        st.markdown('<div class="label">المجموعات</div>', unsafe_allow_html=True)
+        # ===== أيقونة 2 =====
+        c1, c2, c3 = st.columns(3)
+        with c2:
+            if st.button("🗂️"):
+                st.session_state.page = "categories"
+                st.rerun()
+            st.markdown('<div class="label">المجموعات</div>', unsafe_allow_html=True)
 
-        if st.button("👥"):
-            st.session_state.page = "suppliers"
-            st.rerun()
-        st.markdown('<div class="label">الموردين</div>', unsafe_allow_html=True)
+        # ===== أيقونة 3 =====
+        c1, c2, c3 = st.columns(3)
+        with c2:
+            if st.button("👥"):
+                st.session_state.page = "suppliers"
+                st.rerun()
+            st.markdown('<div class="label">الموردين</div>', unsafe_allow_html=True)
 
 # ===== PAGES =====
 else:
